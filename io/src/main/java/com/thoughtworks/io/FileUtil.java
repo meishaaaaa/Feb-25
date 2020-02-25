4172fd3dd97dd29ddf3d171abc1dbfd96444fa5c
meishaaaaa <meishaaaaa@163.com>
2020-02-25 22:41:32 +0800

    change1

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.io;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtil {
 
@@ -13,6 +12,65 @@ public class FileUtil {
      * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
      */
     public static void copyDirectory(File from, File to) throws IOException {
+        //判断源目录是不是一个目录
+        if (!from.isDirectory()) {
+            return;
+        }
+        //如果原目标目录存在，删除目标目录原有文件和文件夹
+        if (to.exists()) {
+            deleteFile(to);
+        }
+        //创建目的目录
+        to.mkdir();
 
+        //获取源目录下的File
+        File[] files = from.listFiles();
+        for (File file : files) {
+            //取得源文件和目标文件的绝对路径，以String的形式
+            String strFrom = from.getAbsolutePath() + File.separator + file.getName();
+            String strTo = to.getAbsolutePath() + File.separator + file.getName();
+
+            //如果file是目录，进行递归
+            if (file.isDirectory()) {
+                File strFrom1 = new File(strFrom);
+                File strTo1 = new File(strTo);
+                copyDirectory(strFrom1, strTo1);
+            }
+            //如果file是文件，调用复制文件的方法
+            if (file.isFile()) {
+                File fromFile = new File(strFrom);
+                File toFile = new File(strTo);
+                copyFile(fromFile, toFile);
+            }
+        }
+    }
+
+
+    public static void copyFile(File fromFile, File toFile) throws IOException {
+        FileInputStream fis = new FileInputStream(fromFile);
+        FileOutputStream fos = new FileOutputStream(toFile);
+
+        byte[] bytes = new byte[1024];
+        int n = 0;
+        while ((n = fis.read(bytes)) != -1) {
+            fos.write(bytes, 0, n);
+        }
+        fis.close();
+        fos.flush();
+        fos.close();
+    }
+
+    public static void deleteFile(File toFile) throws IOException {
+        File[] files = toFile.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                file.delete();
+            } else {
+                deleteFile(file);
+            }
+        }
+        toFile.delete();
     }
 }
+
+
